dd945c92ed79d9ea00d9774ef467c4eb245fa9cb
Wilde <gutopicciani@yahoo.com.br>
2019-01-06 23:19:59 -0200

    Habilitando ServiceWorker e roteamento hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Header from './Header';
 import Home from './Home';
 import About from './About';
